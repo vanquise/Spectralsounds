ea2a7eadb45233c7fe6b74db1e65768f3f24ce6a
vanquise <s.donzo29@gmail.com>
2026-02-08 22:09:53 +0100

    Mobile and Desktop at-a-glance layout fix

--- src/components/LandingPage.jsx
+++ src/components/LandingPage.jsx
@@ -346,7 +346,7 @@ const styles = {
     height: "100%",
     width: "100%",
     maxWidth: "1200px",
-    margin: "7 auto",
+    margin: "15 auto",
     display: "flex",
     flexDirection: "column",
     justifyContent: "center", /* Centers everything vertically */
